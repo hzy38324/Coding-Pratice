182b8fa2a2ca097f4e95d2976234ee7110cbea56
hzy38324 <2668612407@qq.com>
2017-12-04 12:55:58 +0800

    pub

--- CodePractice/src/com/sexycode/codepractice/fibonacci/FibonacciTest.java
+++ CodePractice/src/com/sexycode/codepractice/fibonacci/FibonacciTest.java
@@ -1,32 +1,28 @@
 package com.sexycode.codepractice.fibonacci;
 
-import org.junit.Test;
+import junit.framework.TestCase;
 
 /**
  * @author: hzy
  * @since:
  */
 
-public class FibonacciTest {
-    @Test
+public class FibonacciTest extends TestCase {
     public void test_1() {
         Solution solution = new Solution();
         assert 0 == solution.fibonacci(1);
     }
 
-    @Test
     public void test_2() {
         Solution solution = new Solution();
         assert 1 == solution.fibonacci(2);
     }
 
-    @Test
     public void test_3() {
         Solution solution = new Solution();
         assert 34 == solution.fibonacci(10);
     }
 
-    @Test
     public void test_4() {
         Solution solution = new Solution();
         assert 1 == solution.fibonacci(3);
