e1e80ece689417424ad09940d1cfa341c96af576
hzy38324 <2668612407@qq.com>
2017-12-06 11:33:49 +0800

    pub

---
+++ CodePractice/src/com/sexycode/codepractice/primefactors/PrimeFactors.java
@@ -0,0 +1,46 @@
+package com.sexycode.codepractice.primefactors;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 原题：http://butunclebob.com/ArticleS.UncleBob.ThePrimeFactorsKata
+ * 素因子：https://baike.baidu.com/item/%E8%B4%A8%E5%9B%A0%E5%AD%90/10720836?fr=aladdin
+ * 素数：https://baike.baidu.com/item/%E8%B4%A8%E6%95%B0/263515?fr=aladdin&fromid=115069&fromtitle=%E7%B4%A0%E6%95%B0
+ *
+ * @author: hzy
+ * @since:
+ */
+public class PrimeFactors {
+    public static List<Integer> generate(int n) {
+        if (n <= 1) {
+            return Collections.emptyList();
+        }
+
+        List<Integer> resultList = new ArrayList<>();
+        for (int index = 2; index <= n; index++) {
+            if (isFactor(index, n) && isPrime(index)) {
+                resultList.add(index);
+            }
+        }
+
+        return resultList;
+
+    }
+
+    private static boolean isFactor(int index, int num) {
+        return num % index == 0;
+    }
+
+    private static boolean isPrime(int num) {
+        for (int index = 2; index < num; index++) {
+            if (num % index == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
